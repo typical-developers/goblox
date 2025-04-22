fc6bb7f42e34e1b983c2d70fa7c4d3cd744d4145
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-04-22 12:39:10 -0400

    feat: implement request methods
    i feel like overall these methods could be improved upon.

--- internal/http_client/http_client.go
+++ internal/http_client/http_client.go
@@ -1,6 +1,15 @@
 package http_client
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
 
 type HTTPClient struct {
 	BaseURL string
@@ -11,7 +20,9 @@ type HTTPClient struct {
 func NewHTTPClient(baseURL string) *HTTPClient {
 	return &HTTPClient{
 		BaseURL: baseURL,
-		Client:  http.DefaultClient,
+		Client: &http.Client{
+			Timeout: time.Second * 10,
+		},
 		Headers: map[string]string{},
 	}
 }
@@ -22,4 +33,72 @@ func (c *HTTPClient) SetHeader(key string, value string) {
 
 func (c *HTTPClient) RemoveHeader(key string) {
 	delete(c.Headers, key)
+}
+
+func (c *HTTPClient) BuildRequest(method string, path string, body *interface{}) (*http.Request, error) {
+	url := url.URL{
+		Scheme: "https",
+		Host:   c.BaseURL,
+		Path:   path,
+	}
+
+	if body != nil {
+		json, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+
+		req, err := http.NewRequest(method, url.String(), bytes.NewReader(json))
+		if err != nil {
+			return nil, err
+		}
+
+		return req, nil
+	}
+
+	req, err := http.NewRequest(method, url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (c *HTTPClient) Request(req *http.Request) (result any, err error) {
+	// Set headers
+	if len(c.Headers) > 0 {
+		for k, v := range c.Headers {
+			req.Header.Set(k, v)
+		}
+	}
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		// TODO: Better error handling later.
+		return nil, fmt.Errorf("HTTP error: %s", res.Status)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	contentType := res.Header.Get("Content-Type")
+	if strings.HasPrefix(contentType, "application/json") {
+		println(string(body))
+		err = json.Unmarshal(body, &result)
+
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, nil
+	}
+
+	return result, nil
 }
